edb25d0cd2de51b4533ea1d9f8064e0641e9e973
Renan Leite silva <rleite-s@c3r1p3.42sp.org.br>
2023-05-13 12:01:22 -0300

    including NULL verifier

--- comment.c
+++ comment.c
@@ -5,14 +5,15 @@ char	*find_comment(char *input)
 	char	*start;
 
 	start = input;
-	while (*input)
+	while (input && *input)
 	{
 		if (*input == '\'' || *input == '"')
 			input = ft_strchr(input + 1, *input);
-		if (*input == '#')
+		if (input && *input == '#')
 			if (input == start || ft_isspace(input[-1]))
 				return (input);
-		input++;
+		if (input)
+			input++;
 	}
 	return (NULL);
 }
