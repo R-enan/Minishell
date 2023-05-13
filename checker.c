53e4e20f1c969b3b86d7d8f34161d497618366eb
Renan Leite silva <rleite-s@c1r8p4.42sp.org.br>
2023-05-13 06:54:15 -0300

    checking comments in the command

--- checker.c
+++ checker.c
@@ -2,8 +2,13 @@
 
 int	has_input_error(char *input)
 {
+	char	*comment;
+
 	if (!input)
 		return (1);
+	comment = find_comment(input);
+	if (comment)
+		*comment = 0;
 	if (has_unclosed_quotes(input))
 	{
 		write(1, "Quoting Error\n", 14);
